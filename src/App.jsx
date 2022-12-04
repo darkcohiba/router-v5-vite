d17f42d326575ac0d5556285a31c180a1b512913
Sam <samuel.waters@gmail.com>
2022-12-03 17:20:12 -0700

    finished

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"> 
+            <Home />            
+          </Route>
+          <Route exact path="/about"> 
+            <About />
+          </Route>
+          <Route exact path="/contact"> 
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
