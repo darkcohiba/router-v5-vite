205cc093392ed1322d22bb0b47f1368dd7b07f36
Sam <samuel.waters@gmail.com>
2022-12-06 11:22:23 -0700

    feat: finished adding proxy functionality to vite config

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 function About() {
 
-    fetch("http://localhost:4000/planeteers")
+    fetch("/api/planeteers") //fetching to localhost4000/planeteers
         .then((response) => response.json())
         .then((data) => console.log(data));
     
